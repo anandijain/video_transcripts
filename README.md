a80a867a0c754a1c88ba2424c8d33ee57458a335
anand jain <anandj@uchicago.edu>
2024-10-19 13:05:34 -0700

    add json transcripts and some tokenization distribution

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # transcripts of @the_runofff videos
 
+channel id 
 https://www.youtube.com/channel/UCnKJ-ERcOd3wTpG7gA5OI_g
+
+test vide
+https://www.youtube.com/watch?v=K2OsuCs2YrE
+
+
 ```sh
 yt-dlp --flat-playlist --print "%(id)s" "https://www.youtube.com/channel/UCnKJ-ERcOd3wTpG7gA5OI_g/videos" > video_urls.txt
 yt-dlp --flat-playlist --print "%(id)s" "https://www.youtube.com/channel/UCnKJ-ERcOd3wTpG7gA5OI_g/shorts" > shorts_urls.txt
@@ -9,6 +15,17 @@ cat video_urls.txt shorts_urls.txt streams_urls.txt > all_urls.txt
 yt-dlp --write-info-json --write-auto-subs --skip-download -a all_urls.txt
 ```
 
+
+yt-dlp --write-auto-sub --sub-lang en --skip-download --sub-format json3 "https://www.youtube.com/watch?v=K2OsuCs2YrE"
+yt-dlp --sub-format json3 --write-auto-subs --skip-download -a data/urls/all_urls.txt
+
 todo - just take some # of screenshots or even spaced duration for each video and have llm describe it 
 
 so far i have streamed 872.880948176968 hours
+
+# todo 
+* list which videos are missing vtt/json3 transcripts 
+* add global timestamps to my words for live vids by offseting the transcripts by the live start time 
+    * would give me a way to easily find what words i said on a given day 
+* do the tally on just the utf8s in the segs without join + tokenize 
+* compare with word freqs to charactize how i talk 
--- sum_time.jl
+++ sum_time.jl
@@ -1,10 +1,75 @@
-using JSON3
+using JSON3, CondaPkg, PythonCall, Plots
+tt = pyimport("tiktoken")
+enc = tt.get_encoding("o200k_base")
 
-fns = filter(endswith(".json"), readdir("data/";join=true))
+function parse_segment(s)
+    if !haskey(s, "tOffsetMs")
+        offset = 0
+    else
+        offset = s.tOffsetMs
+    end
+    utf8 = s.utf8
+    (offset, utf8)
+end
 
-js = @. JSON3.read(read(fns))
+function parse_event(e)
+    !haskey(e, "segs") && return []
+    t0 = e.tStartMs
+    pss = parse_segment.(e.segs)
+    map(x -> (x[1] + t0, x[2]), pss)
+end
+
 f(x) = x.formats[1].fragments[1].duration
+get_timestamped_words(j) = reduce(vcat, parse_event.(j.events))
+get_txt_transcipt(timestamped_words::Vector) = join(last.(timestamped_words))
+get_txt_transcipt(j) = get_txt_transcipt(get_timestamped_words(j))
+
+function token_tally(enc, txt)
+    e = enc.encode(txt)
+    v = pyconvert(Vector{Integer}, e)
+    t = tally(v)
+    map(x -> string(enc.decode([x[1]])) => x[2], t)
+end
+
+fn_to_vid_id(s) = last(match(r"\[([^\]]+)\]", s).captures)
+
+fns = filter(endswith(".json"), readdir("data/"; join=true))
+
+js = @. JSON3.read(read(fns));
 durs = []
 
 g, b = goodbad(x -> f(x[2]), zip(fns, js));
 sum(last.(g))
+
+fn = "tmp/i spent 8 HOURS in onshape [K2OsuCs2YrE].en.json"
+
+fns2 = filter(endswith(".json3"), readdir("data/json_transcripts/"; join=true))
+
+# write("eight_hours.txt", join(last.(data)))
+js2 = @. JSON3.read(read(fns2));
+txts = fns2 .=> get_txt_transcipt.(js2);
+
+data = token_tally.(enc, last.(txts));
+ds = Dict.(data)
+m = mergewith(+, ds...)
+bigp = collect(m)
+sortl!(bigp)
+plot(last.(bigp))
+
+
+map(println, first(bigp, 200));
+
+tvids = fn_to_vid_id.(fns2)
+tvid = tvids[1]
+vid_to_mfn(fns, vid) = fns[only(findall(x -> occursin(vid, x), fns))]
+vid = tvid
+fns[only(findall(x -> occursin(vid, x), fns))]
+
+for vid in tvids 
+    try 
+        vid_to_mfn(fns, vid)
+
+    catch e 
+        @show vid
+    end
+end
