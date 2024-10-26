38defb1552aac823bbfc27ef9fb7fce3c87b4d20
anand jain <anandj@uchicago.edu>
2024-10-26 00:14:07 -0700

    find the longest transcripts and look at em

--- sum_time.jl
+++ sum_time.jl
@@ -1,4 +1,8 @@
 using JSON3, CondaPkg, PythonCall, Plots
+Base.haskey(x) = y -> Base.haskey(y, x)
+Base.get(x) = y -> Base.get(y, x)
+# its interesting to think about which functions have a natural way to curry
+# i would never write Base.get(x) = y -> Base.get(x, y)
 tt = pyimport("tiktoken")
 enc = tt.get_encoding("o200k_base")
 
@@ -24,6 +28,9 @@ get_timestamped_words(j) = reduce(vcat, parse_event.(j.events))
 get_txt_transcipt(timestamped_words::Vector) = join(last.(timestamped_words))
 get_txt_transcipt(j) = get_txt_transcipt(get_timestamped_words(j))
 
+get_event_transcript(e) = ((e.tStartMs, e.tStartMs + e.dDurationMs), join(map(x -> x["utf8"], e.segs)))
+get_events_transcript(j) = get_event_transcript.(j.events[2:end])
+
 function token_tally(enc, txt)
     e = enc.encode(txt)
     v = pyconvert(Vector{Integer}, e)
@@ -31,7 +38,8 @@ function token_tally(enc, txt)
     map(x -> string(enc.decode([x[1]])) => x[2], t)
 end
 
-fn_to_vid_id(s) = last(match(r"\[([^\]]+)\]", s).captures)
+# fix this
+get_video_id(s) = s[only(findlast("[", s))+1:only(findlast("]", s))-1]
 
 fns = filter(endswith(".json"), readdir("data/"; join=true))
 
@@ -41,13 +49,30 @@ durs = []
 g, b = goodbad(x -> f(x[2]), zip(fns, js));
 sum(last.(g))
 
-fn = "tmp/i spent 8 HOURS in onshape [K2OsuCs2YrE].en.json"
+fn = "data/json_transcripts/i spent 8 HOURS in onshape [K2OsuCs2YrE].en.json3"
 
 fns2 = filter(endswith(".json3"), readdir("data/json_transcripts/"; join=true))
 
 # write("eight_hours.txt", join(last.(data)))
 js2 = @. JSON3.read(read(fns2));
-txts = fns2 .=> get_txt_transcipt.(js2);
+j = JSON3.read(read(fn))
+evs = j.events[2:end]
+
+tally(haskey("dDurationMs").(evs)) # :/ 
+tally(map(length ∘ get("segs"), evs))
+
+ts = get_txt_transcipt.(js2);
+ids = get_video_id.(fns2)
+txts = ids .=> ts;
+dtxts = Dict(txts)
+longs = sort(txts; by=length ∘ last, rev=true);
+# id K2OsuCs2YrE
+
+encs = enc.encode.(last.(txts))
+
+# need to split by live 
+# also want to know the words per minute on average of streams
+histogram(length.(encs))
 
 data = token_tally.(enc, last.(txts));
 ds = Dict.(data)
@@ -59,17 +84,9 @@ plot(last.(bigp))
 
 map(println, first(bigp, 200));
 
-tvids = fn_to_vid_id.(fns2)
+tvids = get_video_id.(fns2)
 tvid = tvids[1]
 vid_to_mfn(fns, vid) = fns[only(findall(x -> occursin(vid, x), fns))]
 vid = tvid
 fns[only(findall(x -> occursin(vid, x), fns))]
 
-for vid in tvids 
-    try 
-        vid_to_mfn(fns, vid)
-
-    catch e 
-        @show vid
-    end
-end
